5f071e39b397b478338dab894309e24394f4cfcc
seymencanates <teknoturk2345@gmail.com>
2025-04-05 13:16:17 +0300

    We studied and noted the structure of try except

--- basics/try except.js/how to use try except.py
+++ basics/try except.js/how to use try except.py
@@ -1,19 +1,20 @@
 
-
+x = True
 #Single error catching
 try:
     print(x)
 except:
     print("An exception occured")
 
-#In this codeline the error code will show by python because x is not defined
+#In this codeline the error code will show by python because x is not defined(we added it for unmistaken code
+# erase it while trying code)
 
 
 #Multi error catching
 #We can catch as much as error we wanted for
 
 
-x = 23
+x = 238
 
 try:
     print(x)
@@ -22,3 +23,34 @@ except NameError:
 except:
     print("Something went wrong")
 
+#we can also use else too if any error doesnt occurs by program
+
+try:
+    print("My friend is a selfish")
+except:
+    print("A problem have been occured")
+else:
+    print("Any problem didnt occur")
+
+#there is another code type like finally. This code works even except or try code works
+
+#here is an example
+
+try:
+    f = open("data.json" , "r+")
+    try:
+        f.write("Lorem Ipsum")
+    except:
+        print("Something went wrong while writing into the file")
+    finally:
+        f.close()
+except:
+    print("Something went wrong while opening the file")
+
+#we can also throw an exception like on the other languages
+#we uses often raise for this
+
+x = -1
+
+if x< 0:
+    raise Exception("Sorry , no numbers below zero")
